8ee80930eb0be728ec4244fd219854ce56a47859
Yasir-Tariq121 <mtariq@nclouds.com>
2020-07-30 22:30:48 +0500

    files

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['S3Bucket']['name'] = 'yasir-test-bucket.s3-us-west-2.amazonaws.com'
+default['FlinkPkg']['name'] = 'flink-1.11.1-bin-scala_2.11.tgz'
--- recipes/Get_S3.rb
+++ recipes/Get_S3.rb
@@ -3,47 +3,33 @@
 # Recipe:: Get_S3
 #
 
-# bucket_name = "YOUR_BUCKET_NAME"
-# file_name = "FILE_NAME"
-
-#Install java (required for Flink)
-# package 'oracle-java7-installer' do
-#     action :install
-# end
 apt_package 'default-jdk' do
-    action :install # defaults to :install if not specified
+    action :install 
 end
   
   
 #Get S3 package
 remote_file '/flink-1.11.1-bin-scala_2.11.tgz' do
-    source 'https://yasir-test-bucket.s3-us-west-2.amazonaws.com/flink-1.11.1-bin-scala_2.11.tgz'
+    source "https://#{node['S3Bucket']['name']}/#{node['FlinkPkg']['name']}"
     mode '0777'
     action :create
 end
 
-#Extract the Flink package
-# archive_file 'flink-1.11.1' do
-#     path   '/flink-1.11.1-bin-scala_2.11.tgz' 
-#     destination '/'
-#     action :extract
+bash 'flink-1.11.1' do
+    user 'root'
+    cwd '/'
+    code <<-EOH
+      tar -zxf flink-1.11.1-bin-scala_2.11.tgz
+    EOH
+  end
+
+
+  #OR
+
+# execute 'extract_flink' do
+#     command 'tar zxvf /flink-1.11.1-bin-scala_2.11.tgz -C /'
+#     action :run
 # end
-# execute "extract" do
-#     command "sh tar -xvf /flink-1.11.1-bin-scala_2.11.tgz"
-# end
-# bash 'flink-1.11.1' do
-#     user 'root'
-#     cwd '/'
-#     code <<-EOH
-#       tar -zxf flink-1.11.1-bin-scala_2.11.tgz
-#       cd flink-1.11.1/flink-1.11.1/bin
-#       start-cluster.sh
-#     EOH
-#   end
-execute 'extract_flink' do
-    command 'tar zxvf /flink-1.11.1-bin-scala_2.11.tgz -C /'
-    action :run
-end
 
 # Configure Flink
 execute 'flink' do
