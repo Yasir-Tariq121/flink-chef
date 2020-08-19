9b750e327e8b8851fccd9733ace93303529338f0
Yasir-Tariq121 <mtariq@nclouds.com>
2020-08-20 04:47:26 +0500

    files

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['s3Bucket']['name'] = 'aamir-nclouds'
 default['s3Bucket']['key'] = 'flink-1.11.1-bin-scala_2.11.tgz'
-# default['Flink_Updated_Pkg']['name'] = 'Updated-Flink.tgz'
+default['flink']['dir'] = '/flink-1.11.1/bin/'
+default['flink']['user'] = 'root'
