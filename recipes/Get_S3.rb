a5f87e8f323b02e7cfa89ab98ccaa3fad289f5ef
Yasir-Tariq121 <mtariq@nclouds.com>
2020-08-20 04:35:17 +0500

    files

--- recipes/Get_S3.rb
+++ recipes/Get_S3.rb
@@ -3,31 +3,73 @@
 # Recipe:: Get_S3
 #
 
-apt_package 'default-jdk' do
-    action :install 
-end
+# apt_package 'default-jdk' do
+#     action :install 
+# end
   
   
-#Get S3 package
-remote_file '/flink-1.11.1-bin-scala_2.11.tgz' do
-    source "https://#{node['S3Bucket']['name']}/#{node['FlinkPkg']['name']}"
-    mode '0777'
+# #Get S3 package
+# remote_file '/flink-1.11.1-bin-scala_2.11.tgz' do
+#     source "https://#{node['S3Bucket']['name']}/#{node['FlinkPkg']['name']}"
+#     mode '0777'
+#     action :create
+# end
+
+# #Extract package
+# bash 'flink-1.11.1' do
+#     user 'root'
+#     cwd '/'
+#     code <<-EOH
+#       tar -zxf flink-1.11.1-bin-scala_2.11.tgz
+#     EOH
+#   end
+
+
+# # Configure Flink
+# execute 'flink' do
+#     user "root"
+#     command 'sudo bash /flink-1.11.1/bin/start-cluster.sh'
+#     action :run
+# end
+
+package [ 'supervisor', 'default-jdk' ]
+# apt_package 'default-jdk' do
+#     action :install 
+# end
+
+remote_file 'Get_from_S3' do
+    source "https://#{node['s3Bucket']['name']}.s3.amazonaws.com/#{node['s3Bucket']['key']}"
     action :create
+    path '/tmp/flink-1.11.1-bin-scala_2.11.tgz'
 end
 
-#Extract package
-bash 'flink-1.11.1' do
-    user 'root'
-    cwd '/'
-    code <<-EOH
-      tar -zxf flink-1.11.1-bin-scala_2.11.tgz
-    EOH
-  end
+tar_extract '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
+    action     :extract_local
+    target_dir "/"
+    tar_flags [ '-C' ]
+end
 
+template '/etc/systemd/system/flink.service' do
+    source 'flink.service.erb'
+    owner  'root'
+    group  'root'
+    mode   '0644'
+    notifies :run,     'execute[systemd_reload]', :immediately
+end
 
-# Configure Flink
-execute 'flink' do
-    user "root"
-    command 'sudo bash /flink-1.11.1/bin/start-cluster.sh'
-    action :run
+execute 'systemd_reload' do
+    command 'systemctl daemon-reload'
+    action  :nothing
+end
+
+template '/etc/supervisor/conf.d/flink-manager.conf' do
+    source 'flink-manager/supervisor.erb'
+    owner 'root'
+    group 'root'
+    mode '0744'
+end
+
+execute 'supervisor_start_flink' do
+    command 'supervisorctl start flink'
+    action :nothing
 end
--- recipes/update.rb
+++ recipes/update.rb
@@ -9,44 +9,86 @@
   
 
 
-#Stop Flink
-execute 'flink_stop' do
-    user "root"
-    command 'sudo bash /flink-1.11.1/bin/stop-cluster.sh'
-    action :run
-end
-#Get S3 updated package
-remote_file '/Updated-Flink.tgz' do
-    source "https://#{node['S3Bucket']['name']}/#{node['Flink_Updated_Pkg']['name']}"
-    mode '0777'
-    action :create
+# #Stop Flink
+# execute 'flink_stop' do
+#     user "root"
+#     command 'sudo bash /flink-1.11.1/bin/stop-cluster.sh'
+#     action :run
+# end
+# #Get S3 updated package
+# remote_file '/Updated-Flink.tgz' do
+#     source "https://#{node['S3Bucket']['name']}/#{node['Flink_Updated_Pkg']['name']}"
+#     mode '0777'
+#     action :create
+# end
+
+# #Extract package
+# bash 'Updated_Flink' do
+#     user 'root'
+#     cwd '/'
+#     code <<-EOH
+#       tar -zxf Updated-Flink.tgz
+#     EOH
+#   end
+# #Move conf.yaml
+# execute 'move conf' do
+#   user "root"
+#   command 'sudo mv /Updated_Flink/conf/flink-conf.yaml /flink-1.11.1/conf'
+#   action :run
+# end
+# #Move log file
+# execute 'move log' do
+#   user "root"
+#   command 'sudo mv /Updated_Flink/conf/log4j.properties /flink-1.11.1/conf'
+#   action :run
+# end
+
+
+# # Configure Flink
+# execute 'flink' do
+#     user "root"
+#     command 'sudo bash /flink-1.11.1/bin/start-cluster.sh'
+#     action :run
+# end
+
+
+
+package [ 'supervisor', 'unzip' ]
+
+
+execute 'supervisor_stop_flink' do
+    command 'supervisorctl stop flink'
+    action :nothing
 end
 
-#Extract package
-bash 'Updated_Flink' do
-    user 'root'
-    cwd '/'
-    code <<-EOH
-      tar -zxf Updated-Flink.tgz
-    EOH
-  end
-#Move conf.yaml
-execute 'move conf' do
-  user "root"
-  command 'sudo mv /Updated_Flink/conf/flink-conf.yaml /flink-1.11.1/conf'
-  action :run
-end
-#Move log file
-execute 'move log' do
-  user "root"
-  command 'sudo mv /Updated_Flink/conf/log4j.properties /flink-1.11.1/conf'
-  action :run
-end
+# remote_file '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
+#     source "https://#{node['s3Bucket']['name']}.s3.amazonaws.com/#{node['s3Bucket']['key']}"
+#     mode '0777'
+#     action :create
+# end
 
 
-# Configure Flink
-execute 'flink' do
-    user "root"
-    command 'sudo bash /flink-1.11.1/bin/start-cluster.sh'
-    action :run
+# tar_extract '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
+#     action     :extract_local
+# end
+
+template 'Flink_conf' do
+  source 'flink-conf.yaml.erb'
+  owner 'root'
+  group 'root'
+  mode '0777'
+  path '/flink-1.11.1/conf/flink-conf.yaml'
+end
+
+template 'Log4j_conf' do
+  source 'log4j.properties.erb'
+  owner 'root'
+  group 'root'
+  mode '0777'
+  path '/flink-1.11.1/conf/log4j.properties'
+end
+
+execute 'supervisor_start_flink' do
+    command 'supervisorctl start flink'
+    action :nothing
 end
