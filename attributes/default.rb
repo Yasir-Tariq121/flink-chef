464cd6989b3c007e04f2f988f7de9a26227d0a44
Yasir-Tariq121 <mtariq@nclouds.com>
2020-08-20 04:38:19 +0500

    files

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['s3Bucket']['name'] = 'aamir-nclouds.s3-us-west-2.amazonaws.com'
+default['s3Bucket']['name'] = 'aamir-nclouds'
 default['s3Bucket']['key'] = 'flink-1.11.1-bin-scala_2.11.tgz'
 # default['Flink_Updated_Pkg']['name'] = 'Updated-Flink.tgz'
