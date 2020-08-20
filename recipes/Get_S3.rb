7aeb327485d4a90b16966187d0d456ff9041e64d
Yasir-Tariq121 <mtariq@nclouds.com>
2020-08-20 05:32:47 +0500

    files

--- recipes/Get_S3.rb
+++ recipes/Get_S3.rb
@@ -37,10 +37,10 @@ package [ 'supervisor', 'default-jdk' ]
 #     action :install 
 # end
 
-remote_file 'Get_from_S3' do
+remote_file '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
     source "https://#{node['s3Bucket']['name']}.s3.amazonaws.com/#{node['s3Bucket']['key']}"
+    mode '0777'
     action :create
-    path '/tmp/flink-1.11.1-bin-scala_2.11.tgz'
 end
 
 # tar_extract '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
