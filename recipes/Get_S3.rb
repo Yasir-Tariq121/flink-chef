9c9bd2fad1e7af18c2812e300cc2e2ee996cc718
Yasir-Tariq121 <mtariq@nclouds.com>
2020-08-21 00:51:09 +0500

    files

--- recipes/Get_S3.rb
+++ recipes/Get_S3.rb
@@ -3,39 +3,7 @@
 # Recipe:: Get_S3
 #
 
-# apt_package 'default-jdk' do
-#     action :install 
-# end
-  
-  
-# #Get S3 package
-# remote_file '/flink-1.11.1-bin-scala_2.11.tgz' do
-#     source "https://#{node['S3Bucket']['name']}/#{node['FlinkPkg']['name']}"
-#     mode '0777'
-#     action :create
-# end
-
-# #Extract package
-# bash 'flink-1.11.1' do
-#     user 'root'
-#     cwd '/'
-#     code <<-EOH
-#       tar -zxf flink-1.11.1-bin-scala_2.11.tgz
-#     EOH
-#   end
-
-
-# # Configure Flink
-# execute 'flink' do
-#     user "root"
-#     command 'sudo bash /flink-1.11.1/bin/start-cluster.sh'
-#     action :run
-# end
-
 package [ 'supervisor', 'default-jdk' ]
-# apt_package 'default-jdk' do
-#     action :install 
-# end
 
 remote_file '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
     source "https://#{node['s3Bucket']['name']}.s3.amazonaws.com/#{node['s3Bucket']['key']}"
@@ -43,51 +11,7 @@ remote_file '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
     action :create
 end
 
-# tar_extract '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
-#     action     :extract_local
-#     target_dir "/"
-#     tar_flags [ '-C' ]
-# end
-
-# archive_file '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
-#     destination      '/'
-#     mode             '777'
-# end
 execute 'extract_flink' do
     command 'tar zxvf /tmp/flink-1.11.1-bin-scala_2.11.tgz -C /'
     action :run
-end
-
-# #Extract package
-# bash 'flink-1.11.1' do
-#     user 'root'
-#     cwd '/'
-#     code <<-EOH
-#       tar -zxf flink-1.11.1-bin-scala_2.11.tgz
-#     EOH
-#   end
-# end
-template '/etc/systemd/system/flink.service' do
-    source 'flink.service.erb'
-    owner  'root'
-    group  'root'
-    mode   '0644'
-    notifies :run,     'execute[systemd_reload]', :immediately
-end
-
-execute 'systemd_reload' do
-    command 'systemctl daemon-reload'
-    action  :nothing
-end
-
-template '/etc/supervisor/conf.d/flink-manager.conf' do
-    source 'flink-manager/supervisor.erb'
-    owner 'root'
-    group 'root'
-    mode '0744'
-end
-
-execute 'supervisor_start_flink' do
-    command 'supervisorctl start flink'
-    action :nothing
 end
--- recipes/update.rb
+++ recipes/update.rb
@@ -3,75 +3,6 @@
 # Recipe:: update
 #
 
-# apt_package 'default-jdk' do
-#     action :install 
-# end
-  
-
-
-# #Stop Flink
-# execute 'flink_stop' do
-#     user "root"
-#     command 'sudo bash /flink-1.11.1/bin/stop-cluster.sh'
-#     action :run
-# end
-# #Get S3 updated package
-# remote_file '/Updated-Flink.tgz' do
-#     source "https://#{node['S3Bucket']['name']}/#{node['Flink_Updated_Pkg']['name']}"
-#     mode '0777'
-#     action :create
-# end
-
-# #Extract package
-# bash 'Updated_Flink' do
-#     user 'root'
-#     cwd '/'
-#     code <<-EOH
-#       tar -zxf Updated-Flink.tgz
-#     EOH
-#   end
-# #Move conf.yaml
-# execute 'move conf' do
-#   user "root"
-#   command 'sudo mv /Updated_Flink/conf/flink-conf.yaml /flink-1.11.1/conf'
-#   action :run
-# end
-# #Move log file
-# execute 'move log' do
-#   user "root"
-#   command 'sudo mv /Updated_Flink/conf/log4j.properties /flink-1.11.1/conf'
-#   action :run
-# end
-
-
-# # Configure Flink
-# execute 'flink' do
-#     user "root"
-#     command 'sudo bash /flink-1.11.1/bin/start-cluster.sh'
-#     action :run
-# end
-
-
-
-package [ 'supervisor', 'unzip' ]
-
-
-execute 'supervisor_stop_flink' do
-    command 'supervisorctl stop flink'
-    action :nothing
-end
-
-# remote_file '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
-#     source "https://#{node['s3Bucket']['name']}.s3.amazonaws.com/#{node['s3Bucket']['key']}"
-#     mode '0777'
-#     action :create
-# end
-
-
-# tar_extract '/tmp/flink-1.11.1-bin-scala_2.11.tgz' do
-#     action     :extract_local
-# end
-
 template 'Flink_conf' do
   source 'flink-conf.yaml.erb'
   owner 'root'
@@ -88,7 +19,19 @@ template 'Log4j_conf' do
   path '/flink-1.11.1/conf/log4j.properties'
 end
 
-execute 'supervisor_start_flink' do
-    command 'supervisorctl start flink'
-    action :nothing
+template "/etc/supervisord.d/flink.ini" do
+  source "supervisor.ini.erb"
+  mode   0644
+  notifies :run, "execute[supervisor_reload]", :immediately
+  notifies :run, "execute[supervisor_flink_restart]", :immediately
+end
+
+
+execute 'supervisor_flink_restart' do
+  command 'supervisorctl restart all'
+  action :run
+end
+execute 'supervisor_reload' do
+  command 'supervisorctl reload'
+  action :run
 end
