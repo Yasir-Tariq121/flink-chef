bd51da237ae7f744e1e39380275cc6c243e94a53
Yasir-Tariq121 <mtariq@nclouds.com>
2020-08-21 02:22:54 +0500

    files

--- recipes/update.rb
+++ recipes/update.rb
@@ -19,19 +19,27 @@ template 'Log4j_conf' do
   path '/flink-1.11.1/conf/log4j.properties'
 end
 
-template "/etc/supervisor/conf.d/flink.ini" do
-  source "supervisor.ini.erb"
+template "/etc/supervisor/conf.d/flink.conf" do
+  source "supervisor.conf.erb"
   mode   0644
   notifies :run, "execute[supervisor_reload]", :immediately
   notifies :run, "execute[supervisor_flink_restart]", :immediately
+  notifies :run, "execute[supervisor_reread]", :immediately
+  # notifies :run, "execute[supervisor_add]", :immediately
 end
-
-
-execute 'supervisor_flink_restart' do
-  command 'supervisorctl restart all'
+execute 'supervisor_reread' do
+  command 'supervisorctl reread'
+  action :run
+end
+execute 'supervisor_add' do
+  command 'supervisorctl add flink'
   action :run
 end
 execute 'supervisor_reload' do
   command 'supervisorctl reload'
+  action :run
+end
+execute 'supervisor_flink_restart' do
+  command 'supervisorctl restart all'
   action :run
 end
